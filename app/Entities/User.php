d51a4430b49940f02d315b105dac9a7933bb883b
Gustavo Marttos <marttosc@gmail.com>
2017-03-20 22:20:32 -0300

    Fix the User model.

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -3,10 +3,11 @@
 namespace Shed\Entities;
 
 use Illuminate\Notifications\Notifiable;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -14,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'provider', 'provider_id', 'token',
     ];
 
     /**
@@ -23,6 +24,15 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'token',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at', 'updated_at', 'deleted_at',
     ];
 }
