e600e9389034c3f5ac042c1e1f44c13bfffc94c3
Gustavo Marttos <marttosc@gmail.com>
2017-04-18 22:35:54 -0300

    Repository interface updated.

--- app/Repositories/Eloquent/BaseRepository.php
+++ app/Repositories/Eloquent/BaseRepository.php
@@ -3,9 +3,8 @@
 
 namespace Shed\Repositories\Eloquent;
 
+use Exception;
 use Shed\Repositories\Contract\RepositoryInterface;
-use Shed\Repositories\Exceptions\RepositoryException;
-
 use Jenssegers\Mongodb\Eloquent\Model;
 use Illuminate\Container\Container as App;
 
@@ -15,7 +14,6 @@ use Illuminate\Container\Container as App;
  */
 abstract class BaseRepository implements RepositoryInterface
 {
-
     /**
      * @var App
      */
@@ -27,26 +25,25 @@ abstract class BaseRepository implements RepositoryInterface
     protected $model;
 
     /**
-     * @param App $app
-     * @throws \App\Repositories\Exceptions\RepositoryException
+     * @throws Exception
      */
-    public function __construct(App $app) {
-        $this->app = $app;
+    public function __construct()
+    {
+        $this->app = app();
         $this->makeModel();
     }
 
     /**
-     * Specify Model class name
-     *
-     * @return mixed
-     */
-    abstract function model();
-
-    /**
      * @param array $columns
+     * @param array $with
      * @return mixed
      */
-    public function all($columns = ['*']) {
+    public function all($columns = ['*'], $with = [])
+    {
+        if (count($with) > 0) {
+            return $this->model->with($with)->get();
+        }
+
         return $this->model->get($columns);
     }
 
@@ -55,7 +52,8 @@ abstract class BaseRepository implements RepositoryInterface
      * @param array $columns
      * @return mixed
      */
-    public function paginate($perPage = 15, $columns = ['*']) {
+    public function paginate($perPage = 15, $columns = ['*'])
+    {
         return $this->model->paginate($perPage, $columns);
     }
 
@@ -63,7 +61,8 @@ abstract class BaseRepository implements RepositoryInterface
      * @param array $data
      * @return mixed
      */
-    public function create(array $data) {
+    public function create(array $data)
+    {
         return $this->model->create($data);
     }
 
@@ -73,7 +72,8 @@ abstract class BaseRepository implements RepositoryInterface
      * @param string $attribute
      * @return mixed
      */
-    public function update(array $data, $id, $attribute="id") {
+    public function update(array $data, $id, $attribute = "id")
+    {
         return $this->model->where($attribute, '=', $id)->update($data);
     }
 
@@ -81,16 +81,23 @@ abstract class BaseRepository implements RepositoryInterface
      * @param $id
      * @return mixed
      */
-    public function delete($id) {
+    public function delete($id)
+    {
         return $this->model->delete($id);
     }
 
     /**
      * @param $id
      * @param array $columns
+     * @param array $with
      * @return mixed
      */
-    public function find($id, $columns = ['*']) {
+    public function find($id, $columns = ['*'], $with = [])
+    {
+        if (count($with) > 0) {
+            return $this->model->with($with)->find($id, $columns);
+        }
+
         return $this->model->find($id, $columns);
     }
 
@@ -101,19 +108,21 @@ abstract class BaseRepository implements RepositoryInterface
      * @param array $columns
      * @return mixed
      */
-    public function findBy($attribute, $value, $columns = ['*']) {
+    public function findBy($attribute, $value, $columns = ['*'])
+    {
         return $this->model->where($attribute, '=', $value)->first($columns);
     }
 
     /**
      * @return \Illuminate\Database\Eloquent\Builder
-     * @throws RepositoryException
+     * @throws Exception
      */
-    public function makeModel() {
-        $model = $this->app->make($this->model());
+    public function makeModel()
+    {
+        $model = $this->app->make($this->model);
 
         if (!$model instanceof Model)
-            throw new RepositoryException("Class {$this->model()} must be an instance of Jenssegers\\Mongodb\\Eloquent\\Model");
+            throw new Exception("Class {$this->model()} must be an instance of Jenssegers\\Mongodb\\Eloquent\\Model");
 
         return $this->model = $model->newQuery();
     }
