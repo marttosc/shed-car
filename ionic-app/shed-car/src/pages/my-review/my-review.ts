cf387325f7b971c8f557c7b824890cec2b3c27c4
JordanaN <jordanasnogueira@gmail.com>
2017-06-06 16:08:29 -0300

    page MyReview

--- ionic-app/shed-car/src/pages/my-review/my-review.ts
+++ ionic-app/shed-car/src/pages/my-review/my-review.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
-
+import { HttpService } from '../../providers/http-service';
 
 @IonicPage()
 @Component({
@@ -10,15 +10,39 @@ import { HomePage } from '../home/home';
 })
 export class MyReviewPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public myReviews:Array<Object>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public httpService: HttpService) {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MyReview');
+  ngOnInit() {
+    this.list();
   }
 
   back(){
       this.navCtrl.setRoot(HomePage);
+  }
+
+  getMechanist(){
+    // return this.httpService.builder('mechanists/'+ this.mechanist._id + "/reviews")
+    //   .list()
+    //   .then((res) => {
+    //     this.reviews = res.reviews;
+    //   });
+  }
+
+  starClicked(value){
+    return value;
+  }
+
+  list(){
+    return this.httpService.builder('user/reviews')
+      .list()
+      .then((res) => {
+          this.myReviews = res.reviews;
+          console.log(this.myReviews);
+      });
   }
 
 }
