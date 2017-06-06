216033b02a54a5eaaa05ecfddd4cdea7ddc6ad19
JordanaN <jordanasnogueira@gmail.com>
2017-06-06 17:42:25 -0300

    logout

--- ionic-app/shed-car/src/pages/home/home.ts
+++ ionic-app/shed-car/src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MechanistPage } from '../mechanist/mechanist';
 import { MyReviewPage } from '../my-review/my-review';
+import { HttpService } from '../../providers/http-service';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -9,9 +11,23 @@ import { MyReviewPage } from '../my-review/my-review';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public token:string;
 
+  constructor(public navCtrl: NavController, public httpService: HttpService) {
+    this.token = JSON.parse(localStorage['tokens']);
   }
+
+  logout(){
+    return this.httpService.builder('logout')
+      .logout(this.token)
+      .then((res) => {
+        this.token = res;
+        this.navCtrl.setRoot(LoginPage);
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+    }
 
   mechanicalRegistration(){
     this.navCtrl.setRoot(MechanistPage);
